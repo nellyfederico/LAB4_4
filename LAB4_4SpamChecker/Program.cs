09d8207c75edea1dd0f3fe0be435981148a4d549
nellyfederico <nallelyfedericoc@gmail.com>
2020-08-07 16:56:43 -0500

    LAB4_4

---
+++ LAB4_4SpamChecker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LAB4_4SpamChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] blackListWord = 
+            { "pills", "buy", "viagra", "XXX", "free money",
+            "lifetime offer", "send money", "bank account",
+            "nigeria", "online pharmacy", "h8te", "meet girls" };
+
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            message = message.ToLower();
+            for (int i = 0; i < blackList.Length; i++)
+            {
+                if (message.Contains(blackList[i]))
+                {
+                    isSpam = true;
+                }
+            }
+
+
+            if(isSpam == true)
+            { 
+                Console.WriteLine("This email is spam and it has been sent to the Spam Folder!");
+            }
+            else
+            {
+                Console.WriteLine("This email Ok!");
+            }
+        }
+    }
+}
